8dc6a92b72a7d7470029247ef73b01d286155622
xiaohubase <xiaohucode@outlook.com>
2024-04-12 15:02:16 +0800

    reset

--- lib/src/_code_editable.dart
+++ lib/src/_code_editable.dart
@@ -91,7 +91,6 @@ class _CodeEditableState extends State<_CodeEditable> with AutomaticKeepAliveCli
   late CodeIndicatorValueNotifier _codeIndicatorValueNotifier;
   bool _touchMove = false;
   bool _disposed = false;
-  late Offset _touchPointer;
 
   @override
   bool get wantKeepAlive => widget.focusNode.hasFocus;
@@ -261,12 +260,8 @@ class _CodeEditableState extends State<_CodeEditable> with AutomaticKeepAliveCli
           return false;
         },
         child: Platform.isAndroid || Platform.isIOS ? Listener(
-          onPointerDown: (event) {
-            _touchPointer = event.position;
-          },
-          onPointerUp: (event) {
-            if (!_touchMove ||
-                _touchPointer.isSamePosition(event.position)) {
+          onPointerUp: (_) {
+            if (!_touchMove) {
               _inputController.ensureInput();
             }
             _touchMove = false;
