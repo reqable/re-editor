70eb7ba6fe16541398bc008a96c054a71e388dab
Litt1eQ <93601664@qq.com>
2024-04-23 18:29:06 +0800

    fix: change _consts.dart 1. kIsWeb -> defaultTargetPlatform. (The component's behavior on the browser side is consistent with the corresponding platform) 2. Remove useless const vars.

--- lib/src/_consts.dart
+++ lib/src/_consts.dart
@@ -1,10 +1,5 @@
 part of re_editor;
 
-final kIsMacOS = !kIsWeb && Platform.isMacOS;
-final kIsWindows = !kIsWeb && Platform.isWindows;
-final kIsLinux = !kIsWeb && Platform.isLinux;
-final kIsAndroid = !kIsWeb && Platform.isAndroid;
-final kIsIOS = !kIsWeb && Platform.isIOS;
-final kIsApple = !kIsWeb && (Platform.isIOS || Platform.isMacOS);
-final kIsMobile = !kIsWeb && (Platform.isIOS || Platform.isAndroid);
-final kIsDesktop = !kIsWeb && (Platform.isMacOS || Platform.isWindows || Platform.isLinux);
+final kIsMacOS = defaultTargetPlatform == TargetPlatform.macOS;
+final kIsAndroid = defaultTargetPlatform == TargetPlatform.android;
+final kIsIOS = defaultTargetPlatform == TargetPlatform.iOS;
