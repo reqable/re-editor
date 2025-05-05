5e1a11b31bdba7fcd780e54cd3cb0fa9875ee45e
Steve Gehrman <sgehrman@gmail.com>
2025-05-05 20:16:37 +0900

    sdf

--- lib/src/_isolate.dart
+++ lib/src/_isolate.dart
@@ -4,35 +4,26 @@ typedef IsolateRunnable<Req, Res> = Res Function(Req req);
 typedef IsolateCallback<Res> = void Function(Res res);
 
 class _IsolateTasker<Req, Res> {
-  _IsolateTasker(this.name, IsolateRunnable<Req, Res> runnable) {
+  _IsolateTasker(this.name, this.runnable) {
     _closed = false;
-    _isolateManager = IsolateManager.create(runnable);
   }
   final String name;
   late bool _closed;
-
-  late IsolateManager<Res, Req>? _isolateManager;
+  IsolateRunnable<Req, Res> runnable;
 
   Future<void> run(Req req, IsolateCallback<Res> callback) async {
     if (_closed) {
       return;
     }
-    await _isolateManager?.compute(
-      req,
-      callback: (message) {
-        if (_closed) {
-          return false;
-        }
-        callback(message);
-
-        return true;
-      },
-    );
+    final res = runnable(req);
+    if (_closed) {
+      return;
+    }
+    print('IsolateTasker: $name');
+    callback(res);
   }
 
   void close() {
     _closed = true;
-    _isolateManager?.stop();
-    _isolateManager = null;
   }
 }
